6420d494fb0ab4f9ab71eb6eb9728f00d86126cd
Erwan Peres <eperes@jebidouille.com>
2024-11-29 15:39:14 -0500

    ajout du dashboard user et de la redirection au login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!  # Protection Devise
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
@@ -7,5 +7,29 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address])
+  end
+  
+  def after_sign_in_path_for(resource)
+    # Options les plus courantes :
+    
+    # 1. Redirection vers le tableau de bord
+    dashboard_path
+    
+  
+    # 2. Redirection vers une page spécifique selon le type d'utilisateur
+    # case resource
+    # when Admin
+    #  admin_dashboard_path
+    # when User
+    # user_dashboard_path
+    #else
+    #  root_path
+    #end
+  
+    # 3. Redirection vers la page précédente
+    # stored_location_for(resource) || root_path
+  
+    # 4. Redirection avec des paramètres personnalisés
+    # params[:redirect_to] || root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
   root 'pages#home'
   get 'about', to: 'pages#about'
+  
+  get 'dashboard', to: 'dashboard#index', as: :dashboard
   
   devise_for :users
 end
